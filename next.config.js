f1b7dfa0c84d07e5bc2e9b6f49ca673e42f2d306
Your Name <your.email@example.com>
2024-11-17 13:02:34 +0000

    Lets deploy it again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-};
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
