b4893076f11b22c88b73392b20e2923ea56910cc
Your Name <your.email@example.com>
2024-11-18 01:55:45 +0000

    ggg

--- components/booking/LocationSelector.tsx
+++ components/booking/LocationSelector.tsx
@@ -11,15 +11,15 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { MapPin } from 'lucide-react'
 
 const locations = [
-  { id: 'bedford', name: 'Bedford', slug: 'bedford-mk40', postcode: 'MK40 1UH' },
-  { id: 'birmingham', name: 'Birmingham', slug: 'birmingham-b66', postcode: 'B66 4TB' },
-  { id: 'bolton', name: 'Bolton', slug: 'bolton-bl6', postcode: 'BL6 4RQ' },
-  { id: 'cambridge', name: 'Cambridge', slug: 'cambridge-cb1', postcode: 'CB1 9XQ' },
-  { id: 'hertfordshire', name: 'Hertfordshire', slug: 'hertfordshire-sg5', postcode: 'SG5 1AR' },
-  { id: 'newcastle', name: 'Newcastle Under Lyme', slug: 'newcastle-st5', postcode: 'ST5 2JG' },
-  { id: 'peterborough', name: 'Peterborough', slug: 'peterborough-pe2', postcode: 'PE2 6XU' },
-  { id: 'plymouth', name: 'Plymouth', slug: 'plymouth-pl9', postcode: 'PL9 9JB' },
-  { id: 'southampton', name: 'Southampton', slug: 'southampton-so16', postcode: 'SO16 4NW' }
+  { id: '1', name: 'Bedford', slug: 'bedford-mk40', postcode: 'MK40 1UH' },
+  { id: '2', name: 'Birmingham', slug: 'birmingham-b66', postcode: 'B66 4TB' },
+  { id: '3', name: 'Bolton', slug: 'bolton-bl6', postcode: 'BL6 4RQ' },
+  { id: '4', name: 'Cambridge', slug: 'cambridge-cb1', postcode: 'CB1 9XQ' },
+  { id: '5', name: 'Hertfordshire', slug: 'hertfordshire-sg5', postcode: 'SG5 1AR' },
+  { id: '6', name: 'Newcastle Under Lyme', slug: 'newcastle-st5', postcode: 'ST5 2JG' },
+  { id: '7', name: 'Peterborough', slug: 'peterborough-pe2', postcode: 'PE2 6XU' },
+  { id: '8', name: 'Plymouth', slug: 'plymouth-pl9', postcode: 'PL9 9JB' },
+  { id: '9', name: 'Southampton', slug: 'southampton-so16', postcode: 'SO16 4NW' }
 ]
 
 interface LocationSelectorProps {
