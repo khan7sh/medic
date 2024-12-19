bc2489cae23cd8b4958e1c1c021b494d2b5e832a
Your Name <your.email@example.com>
2024-12-19 02:29:57 +0000

    error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,19 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/supabase'
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
-}
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
-}
-
+// Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
   {
     auth: {
-      persistSession: false
+      persistSession: false,
+      detectSessionInUrl: false,
+      autoRefreshToken: false
     }
   }
 ) 
