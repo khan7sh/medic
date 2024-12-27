7a22afb0216792c2481b22bb903f146d0ea4f0e5
Your Name <your.email@example.com>
2024-12-27 21:39:49 +0000

    error

--- next.config.js
+++ next.config.js
@@ -15,6 +15,15 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false
+      }
+    }
+    return config
   }
 }
 
